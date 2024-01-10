c3f011a82335a51a6c593ee76683bb94ae64ae1d
baltar9 <koonn8657@gmail.com>
2024-01-10 20:17:24 -0300

    4

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,13 +46,34 @@
 //     }
 // })
 
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon grass">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
 
-fetch(url)
-.then((response) => response.json())
-.then((jsonBody) => console.log(jsonBody))
-.catch((error) => console.log('error'))
+            <div class="detail">
+                <ol class="types">
+                    <li class="type grass">grass</li>
+                    <li class="type poison">poison</li>
+                </ol> 
+
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+                    alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+
+pokeAPI.getPokemons().then((pokemons) => {
+     for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        pokemonList.innerHTML += convertPokemonToLi(pokemon) 
+    }
+})
+
 
 
